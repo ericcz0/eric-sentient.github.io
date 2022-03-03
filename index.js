dff4330da6949722371347387be3074d6badeca8
eric-sentient <eric-sentient@berkeley.edu>
2022-03-03 00:51:31 -0800

    added css, content and resources

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function showContent(name) {
     contents = ['home', 'projects', 'music', 'contact']
     for (const item of contents) {
         document.getElementById(item).style.display = "none";
+        document.getElementById('nav_'+item).classList.remove("active");
     }
     document.getElementById(name).style.display = "block";
+    document.getElementById('nav_'+name).classList.add("active");
 }
